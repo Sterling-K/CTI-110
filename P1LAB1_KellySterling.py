313334d0adcbf72c4879534a87a3ecefb509d08b
Sterling-K <kellys2181@student.faytechcc.edu>
2025-11-10 16:45:24 -0500

    Add files via upload

---
+++ P1LAB1_KellySterling.py
@@ -0,0 +1,8 @@
+# Sterling Kelly
+# November 10, 2025
+# P1LAB1
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("Hello,", first_name, last_name + "! Welcome to CTI-110")
